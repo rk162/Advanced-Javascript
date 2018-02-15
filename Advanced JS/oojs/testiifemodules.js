90ebb840b9b8b0870d5238030e61ade95b5c6317
Ritesh Kumar <rku202@sapient.com>
2018-02-15 19:56:09 +0530

    Removed a folder

--- Advanced JS/oojs/testiifemodules.js
+++
@@ -1,9 +0,0 @@
-// require js which uses AMD (Asynchronous Module Definition)
-// const iife=require('./moduleA/testmodA');
-
-// ES 2015 way of importing modules
-
-import iife from './moduleA/testmodAes2015'
-
-console.log(iife.methodA().getDay())
-console.log(iife.methodB().getMonth());
--- Advanced JS/oojs/testmoduleA.js
+++
@@ -1,7 +0,0 @@
-const { Customer, Order } = require('./moduleA/Customer');
-
-// let customer = new Customer(1, "Some Name");
-
-let order = new Order(2, "Order1")
-console.log('Customer Details ' + Customer);
-console.log('Order Details ' + order);
