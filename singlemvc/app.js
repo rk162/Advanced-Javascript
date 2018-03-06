90ce7b0317885df007e550601cb1630077bc2677
Ritesh Kumar 7 <rku202@sapient.com>
2018-03-06 16:03:50 +0530

    c

--- singlemvc/app.js
+++ singlemvc/app.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/mean')
-  .then(() =>  console.log('connection succesful'))
+  .then(() =>  console.log('connection successful'))
   .catch((err) => console.error(err));
 
 var index = require('./routes/index');
